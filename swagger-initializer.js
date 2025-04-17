22f36dff0cd81536fdec0a7740cab3534a61137a
Sevdaccccc <sevdapoladova23@gmail.com>
2025-04-18 02:29:04 +0400

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,20 +1,12 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+window.onload = () => {
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "CRM-REST-API.yaml",  // Burada CRM-REST-API.yaml faylını göstərirsən
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
     layout: "StandaloneLayout"
   });
-
-  //</editor-fold>
 };
